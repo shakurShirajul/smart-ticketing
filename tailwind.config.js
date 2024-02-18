858d88679b4afbcf8671f3250ddf962de8be2091
shakurShirajul <siirajulisllam@gmail.com>
2024-02-18 16:09:53 +0600

    banner added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "light"
+  },
+}
+
