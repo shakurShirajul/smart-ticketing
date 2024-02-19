72c8a7b2be9c7614f919b024f5492efbad44f1f5
shakurShirajul <siirajulisllam@gmail.com>
2024-02-19 11:21:35 +0600

    Almost Complete

--- script/smartTicket.js
+++ script/smartTicket.js
@@ -5,23 +5,29 @@ const totalPrice = document.getElementById('total-price');
 const grandPrice = document.getElementById('grand-price');
 const couponAddButton = document.getElementById('coupon-apply-button');
 const couponInput = document.getElementById('coupon-input');
-const couponBox   = document.getElementById('coupon-box');
-const couponDiv = document.getElementById('coupon-div')
+const couponBox = document.getElementById('coupon-box');
+const couponDiv = document.getElementById('coupon-div');
+const totalSeatNumber = document.getElementById('total-seat-number');
 const newCouponBox = couponBox;
-
+const sendButton = document.getElementById('send-button');
+const nameInput = document.getElementById('name-input');
+const numberInput = document.getElementById('number-input');
+const mainElement = document.getElementById('main-element');
+const secondPage = document.getElementById('second-page');
+const continueButton = document.getElementById('continue-button')
 let flag = false;
-// console.log(setaCatalogue);
+let flag1 = false;
+
 
 // Selecting Ticket 
 setaCatalogue.addEventListener('click', function (event) {
 
-    // console.log(event.target.tagName);
     if (event.target.tagName == 'BUTTON') {
-        // console.log("yes");
-        // numberOfSeatSelected.innerText = 1;
+
         const button = event.target;
         const checkingButton = button.classList.contains('selected');
         let numberOfSeatSelected = parseInt(numberOfSeatSelectedElements.innerText);
+
 
 
         if (checkingButton == false && numberOfSeatSelected < 4) {
@@ -30,18 +36,23 @@ setaCatalogue.addEventListener('click', function (event) {
             numberOfSeatSelectedElements.innerText = numberOfSeatSelected;
             createElement(button.innerText)
             calculateTotalPrice(numberOfSeatSelected);
+            totalSeatNumber.innerText = 40 - numberOfSeatSelected;
         } else if (checkingButton == true) {
             button.classList.remove('selected');
+            deleteElement(button.innerText);
             numberOfSeatSelected -= 1;
             numberOfSeatSelectedElements.innerText = numberOfSeatSelected;
-            deleteElement(button.innerText);
-            if(flag==true){
+            console.log(numberOfSeatSelected)
+            totalSeatNumber.innerText = 40 - numberOfSeatSelected;
+            if (flag == true) {
                 couponDiv.appendChild(newCouponBox);
-                flag=false;
+                document.getElementById('total-discount-amount').remove();
+                flag = false;
             }
+        } else {
+            alert("You Already Selected Four Seat");
         }
-        console.log(numberOfSeatSelected)
-        // console.log(numberOfSeatSelected);
+
         if (numberOfSeatSelected == 4) {
             // Enable Add Button
             couponAddButton.removeAttribute('disabled');
@@ -52,23 +63,69 @@ setaCatalogue.addEventListener('click', function (event) {
                 couponAddButton.setAttribute('disabled', "")
             }
         }
+        if (numberOfSeatSelected > 0) {
+            sendButton.removeAttribute('disabled', "");
+        }
+        else {
+            sendButton.setAttribute('disabled', "");
+        }
     }
 })
 
 
 //  Coupon Add Button
-couponAddButton.addEventListener('click', function(event){
+couponAddButton.addEventListener('click', function (event) {
     let couponCode = couponInput.value;
-    if(couponCode == 'Couple 20'){
-        grandPrice.innerText = parseInt(grandPrice.innerText) - parseInt(grandPrice.innerText)*0.2;
+    if (couponCode == 'Couple 20') {
+        grandPrice.innerText = parseInt(grandPrice.innerText) - parseInt(grandPrice.innerText) * 0.2;
+
+        const newDiscountElement = document.createElement('div');
+        const newDiscountParagraph = document.createElement('p');
+        const newDiscountPrice = document.createElement('p');
+
+        newDiscountParagraph.innerText = "Discount";
+        newDiscountPrice.innerText = parseInt(totalPrice.innerText) * 0.2
+
+        newDiscountElement.appendChild(newDiscountParagraph);
+        newDiscountElement.appendChild(newDiscountPrice);
+
+        newDiscountElement.classList.add('flex')
+        newDiscountElement.classList.add('justify-between')
+        newDiscountElement.classList.add('my-4')
+
+        newDiscountElement.setAttribute("id", "total-discount-amount")
+
         couponBox.remove();
+        couponDiv.appendChild(newDiscountElement);
         flag = true;
     }
-    else if(couponCode == 'NEW15'){
-        grandPrice.innerText = parseInt(grandPrice.innerText) - parseInt(grandPrice.innerText)*0.15;
+    else if (couponCode == 'NEW15') {
+        grandPrice.innerText = parseInt(grandPrice.innerText) - parseInt(grandPrice.innerText) * 0.15;
+
+        const newDiscountElement = document.createElement('div');
+        const newDiscountParagraph = document.createElement('p');
+        const newDiscountPrice = document.createElement('p');
+
+        newDiscountParagraph.innerText = "Discount";
+        newDiscountPrice.innerText = parseInt(totalPrice.innerText) * 0.15;
+
+        newDiscountElement.appendChild(newDiscountParagraph);
+        newDiscountElement.appendChild(newDiscountPrice);
+
+        newDiscountElement.classList.add('flex')
+        newDiscountElement.classList.add('justify-between')
+        newDiscountElement.classList.add('my-4')
+
+        newDiscountElement.setAttribute("id", "total-discount-amount")
+
         couponBox.remove();
+        couponDiv.appendChild(newDiscountElement);
         flag = true;
-        console.log(couponBox);
+
+    }
+    else {
+        flag1 = true;
+        window.alert("Wrong Coupon Code");
     }
 })
 
@@ -91,12 +148,14 @@ function deleteElement(seatName) {
 
     const setaListElement = newSeatList.children;
 
-    const n = newSeatList.childElementCount
-
+    const n = parseInt(numberOfSeatSelectedElements.innerText);
+    console.log(n, seatName);
     for (let i = 0; i < n; i++) {
         if (setaListElement[i].children[0].innerText == seatName) {
+            console.log(setaListElement[i].children[0].innerText);
             setaListElement[i].remove();
             minusTakaFromTotalTaka();
+            break;
         }
     }
 }
@@ -122,5 +181,17 @@ function createElement(seatName) {
     newDiv.classList.add('my-4')
 
     seatNumberList.appendChild(newDiv);
-    console.log(newDiv);
 }
+// Send Button
+sendButton.addEventListener('click', function (event) {
+    mainElement.classList.add('hidden');
+    secondPage.classList.remove('hidden');
+    nameInput.value = '';
+    numberInput.value = '';
+})
+
+// Continue Button
+continueButton.addEventListener('click', function () {
+    secondPage.classList.add('hidden');
+    mainElement.classList.remove('hidden');
+})
